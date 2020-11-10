66f00e61c3141ef718f1a96388082845ad307a13
iBlurBlur <tanakorn.ngam@gmail.com>
2020-11-10 18:05:57 +0700

    get method

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 
-app.get('/say', function (req, res) {
-    res.send('Hello Cat')
+app.get('/say1', function (req, res) {
+    res.send('Cat Lover v1')
 })
+
+// localhost:3000/say/tanakorn/30
+app.get('/say/:name/:age', function (req, res) {
+    res.send(`hello ${req.params.name}, ${req.params.age}`)
+})
+
+// localhost:3000/search?name=macbook&price=999
+app.get('/search', function (req, res) {
+    res.send(`search: ${req.query.name}, ${req.query.price}`)
+})
+
+
 
 const port = 3000
 
