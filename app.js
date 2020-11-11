2dd5126da2e7e28d04321f488de8e2e68f6f5b79
iBlurBlur <tanakorn.ngam@gmail.com>
2020-11-11 12:14:34 +0700

    response product and using rest client on vscode

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 const express = require('express')
 const app = express()
 
-app.get('/say1', function (req, res) {
-    res.send('Cat Lover v1')
+class Product {
+    constructor(id, name, image, price, stock) {
+        this.id = id
+        this.name = name
+        this.image = image
+        this.price = price
+        this.stock = stock
+    }
+}
+
+const products = [
+    new Product(1, "Macbook Pro", "", 90000, 0),
+    new Product(2, "iPhone XS", "", 50000, 10),
+]
+
+app.get('/products', (req, res) => {
+    res.json(products)
 })
 
-// localhost:3000/say/tanakorn/30
-app.get('/say/:name/:age', function (req, res) {
-    res.send(`hello ${req.params.name}, ${req.params.age}`)
+app.get('/products/price', (req, res) => {
+    const { min, max } = req.query;
+    const result = products.filter(product => product.price >= min && product.price <= max)
+    res.json(result)
 })
 
-// localhost:3000/search?name=macbook&price=999
-app.get('/search', function (req, res) {
-    res.send(`search: ${req.query.name}, ${req.query.price}`)
+app.get('/products/:id', (req, res) => {
+    const result = products.filter(product => product.id == req.params.id)
+    if (result.length > 0) {
+        res.json(result[0])
+    } else {
+        res.status(404).json({})
+    }
 })
 
 
 
 const port = 3000
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
     console.log("Press Ctrl + C to quit.");
 })
